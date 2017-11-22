28f5e7f6a816e6c53ae339eb249c03a2c2af1a52
shawnking07 <shawnking07@outlook.com>
2017-11-22 15:42:54 +0800

    修改逻辑

--- src/main/java/com/example/carrentsys/repository/CarRepository.java
+++ src/main/java/com/example/carrentsys/repository/CarRepository.java
@@ -2,7 +2,7 @@ package com.example.carrentsys.repository;
 
 import com.example.carrentsys.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,11 +12,14 @@ public interface CarRepository extends JpaRepository<Car, Integer> {
 
     List<Car> findAll();
 
-    List<Car> findByStatus(@Param("status") Car.Status status);
+    List<Car> findByStatus(Car.Status status);
 
     boolean existsByLicensePlate(String licensePlate);
 
     Car findByLicensePlate(String licensePlate);
 
     int countByStatus(Car.Status status);
+
+    @Query(value = "SELECT c FROM Car c WHERE c.price<=?1 OR c.price>=?2")
+    List<Car> findByPriceOutof(int start, int end);
 }
