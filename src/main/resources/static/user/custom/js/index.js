0a079b3c8d41552e4231b06cc2b63f25153e77b1
nitmali <32920712+nitmali@users.noreply.github.com>
2017-12-13 13:31:39 +0800

    修复cookie报错
    Signed-off-by: nitmali <32920712+nitmali@users.noreply.github.com>

--- src/main/resources/static/user/custom/js/index.js
+++ src/main/resources/static/user/custom/js/index.js
@@ -3,24 +3,8 @@ $(document).ready(function () {
     $('[data-toggle="popover"]').popover();
     footerhtml();
     $(".getlogin").hide();
-    user();
     focus();
 });
-
-function user() {
-    $.get("/getusername",
-        function (data) {
-            if (data.msg !== "error") {
-                if ((data.usertype === "client") && (data.username === $.cookie("username"))) {
-                    $(".myself").html(data.username);
-                    $(".notlogin").hide();
-                    $(".getlogin").show();
-                    $("#loginmodal").modal("hide")
-                }
-            }
-        }, "json"
-    );
-}
 
 function focus() {
     if ($("#inputUsername").val() === "") {
--- src/main/resources/static/user/custom/js/profile.js
+++ src/main/resources/static/user/custom/js/profile.js
@@ -1,18 +1,87 @@
-$('iframe').hover(
-    function () {
-        SetCwinHeight();
-    }
-);
+// $('iframe').hover(
+//     function () {
+//         SetCwinHeight();
+//     }
+// );
+//
+// function SetCwinHeight() {
+//     var iframeid = document.getElementById("iframeid"); //iframe id
+//     if (document.getElementById) {
+//         if (iframeid && !window.opera) {
+//             if (iframeid.contentDocument && iframeid.contentDocument.body.offsetHeight) {
+//                 iframeid.height = iframeid.contentDocument.body.offsetHeight;
+//             } else if (iframeid.Document && iframeid.Document.body.scrollHeight) {
+//                 iframeid.height = iframeid.Document.body.scrollHeight;
+//             }
+//         }
+//     }
+// }
 
-function SetCwinHeight() {
-    var iframeid = document.getElementById("iframeid"); //iframe id
-    if (document.getElementById) {
-        if (iframeid && !window.opera) {
-            if (iframeid.contentDocument && iframeid.contentDocument.body.offsetHeight) {
-                iframeid.height = iframeid.contentDocument.body.offsetHeight;
-            } else if (iframeid.Document && iframeid.Document.body.scrollHeight) {
-                iframeid.height = iframeid.Document.body.scrollHeight;
-            }
+var table = $("#order_table").DataTable({
+    language: {
+        "sProcessing": "处理中...",
+        "sLengthMenu": "显示 _MENU_ 项结果",
+        "sZeroRecords": "没有匹配结果",
+        "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+        "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+        "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+        "sInfoPostFix": "",
+        "sSearch": "搜索:",
+        "sUrl": "",
+        "sEmptyTable": "表中数据为空",
+        "sLoadingRecords": "载入中...",
+        "sInfoThousands": ",",
+        "oPaginate": {
+            "sFirst": "首页",
+            "sPrevious": "上页",
+            "sNext": "下页",
+            "sLast": "末页"
+        },
+        "oAria": {
+            "sSortAscending": ": 以升序排列此列",
+            "sSortDescending": ": 以降序排列此列"
         }
-    }
-}
+    },
+    responsive: true,
+    bAutoWidth: true,
+    processing: true,
+    searching: false,
+    bSort: false,
+    ajax: {
+        url: '/historyRenting',
+        type: 'get',
+        datatype: 'json',
+        dataSrc: ''
+    },
+    columns: [
+        {data: 'id'},
+        {data: 'car.licensePlate'},
+        {
+            data: function (obj) {
+                if (obj.lendStartTime === null) return null;
+                return obj.lendStartTime;
+            }
+        },
+        {
+            data: function (obj) {
+                if (obj.lendEndTime === null) return null;
+                return obj.lendEndTime;
+            }
+        },
+        {data: 'submitTime'},
+        {
+            data: function (obj) {
+                if (obj.approvalTime === null) return null;
+                return obj.approvalTime;
+            }
+        },
+        {
+            data: function (obj) {
+                if (obj.admin === null) return null;
+                return obj.admin.username;
+            }
+        },
+        {data: 'amount'},
+        {data: 'status'}
+    ]
+});
